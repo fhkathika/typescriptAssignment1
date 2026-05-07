f00eeae07f577fd4332ec2e5fb7211870c97580a
fhkathika <fhkathika057@gmail.com>
2026-05-07 23:49:39 +0600

    added code

--- src/solutions.ts
+++ src/solutions.ts
@@ -94,3 +94,11 @@ class Student extends Person{
 
 const student = new Student("Alice", 20, "A");
 console.log(student.getDetails()) ;
+
+// Problem 7 
+// Create a function getIntersection that takes two arrays of numbers and returns a new array containing only the elements that are present in both arrays.
+const getIntersection=(arr1:number[],arr2:number[])=>{
+ return arr1.filter(item=>arr2.includes(item))
+}
+console.log(getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7])
+)
